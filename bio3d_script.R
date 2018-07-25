96945a09e23ba67490140d45ac5abe7b384562aa
PT Dolan <ptdolan@stanford.edu>
2018-07-25 15:16:46 -0700

    Updated NMA script to run on generic GitHub folder

--- bio3d_script.R
+++ bio3d_script.R
@@ -1,12 +1,9 @@
 library(bio3d)
-PDB<-read.pdb2(file = "~/hsp90ab_5fwk.pdb")
-PDBnma<-aanma.pdb(pdb = PDB)
-
-for (file in c("~/PHE-508-ALA_3pry.pdb.pdb",
-"~/PHE-508-ILE_3pry.pdb.pdb",
-"~/PHE-508-SER_3pry.pdb.pdb",
-"~/PHE-508-MET_3pry.pdb.pdb")){
-  mutPDB<-read.pdb2(file = file)
-  mutPDBnma<-aanma.pdb(mutPDB,rm.wat = T)
-  pdf(plot(nma.pdb,main=file))
+for (f in list.files("~/GitHub/bio3d_nma/Structures/",pattern = ".pdb")){
+  mutPDB<-read.pdb2(file = f)
+  mutPDBnma<-aanma.pdb(mutPDB)
+  
+  pdf(paste(file,".pdf"))
+  plot(nma.pdb,main=file)
+  dev.off()
 }
